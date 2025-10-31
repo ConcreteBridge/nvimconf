fe9f338d79edb113b50e263d4b6ac2d55394aadb
ConcreteBridge <conradspark@gmail.com>
2025-10-31 14:25:16 -0500

    update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,64 +1,14 @@
 local map = vim.keymap.set
 
-map(
-  "n",
-  "<leader>f",
-  function () require("fzf-lua").files() end,
-  { desc = "files" }
-)
-
-map(
-  "n",
-  "<leader>b",
-  function () require("fzf-lua").buffers() end,
-  { desc = "buffers" }
-)
-
-map(
-  "n",
-  "<leader>g",
-  function () require("fzf-lua").live_grep_native() end,
-  { desc = "grep" }
-)
-
-map(
-  "v",
-  "<leader>g",
-  function () require("fzf-lua").grep_cword() end,
-  { desc = "grep word" }
-)
-
-map(
-  "n",
-  "<leader>l",
-  function () require("fzf-lua").lines() end,
-  { desc = "lines" }
-)
-
-map(
-  "n",
-  "<leader>o",
-  function () require("fzf-lua").oldfiles() end,
-  { desc = "oldfiles" }
-)
-
-map(
-  "n",
-  "<leader>z",
-  function () require("fzf-lua").zoxide() end,
-  { desc = "zoxide" }
-)
+map("n", "<leader>f", function () require("fzf-lua").files() end, { desc = "files" })
+map("n", "<leader>b", function () require("fzf-lua").buffers() end, { desc = "buffers" })
+map("n", "<leader>g", function () require("fzf-lua").live_grep_native() end, { desc = "grep" })
+map("v", "<leader>g", function () require("fzf-lua").grep_cword() end, { desc = "grep word" })
+map("n", "<leader>l", function () require("fzf-lua").lines() end, { desc = "lines" })
+map("n", "<leader>o", function () require("fzf-lua").oldfiles() end, { desc = "oldfiles" })
+map("n", "<leader>z", function () require("fzf-lua").zoxide() end, { desc = "zoxide" })
 
 map("n", "<leader>t", "", { desc = "+Terminal" })
-map(
-  "n",
-  "<leader>tj",
-  function () require("FTerm").scratch { cmd = { "just" } } end,
-  { desc = "just" }
-)
-map(
-  "n",
-  "<leader>tl",
-  function () require("FTerm").scratch { cmd = { "lazygit" } } end,
-  { desc = "lazygit" }
-)
+map("n", "<leader>tt", function () require("FTerm").toggle() end, { desc = "term" })
+map("n", "<leader>tj", function () require("FTerm").scratch { cmd = { "just" } } end, { desc = "just" })
+map("n", "<leader>tl", function () require("FTerm").scratch { cmd = { "lazygit" } } end, { desc = "lazygit" })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,12 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
 add { source = "ibhagwan/fzf-lua" }
-later(
-  function ()
-    require("fzf-lua").setup {
-      "fzf-vim",
-      fzf_colors = true,
-    }
-  end
-)
+later(function ()
+  require("fzf-lua").setup {
+    "fzf-vim",
+    fzf_colors = true,
+  }
+end)
 
 add {
   source = "saghen/blink.cmp",
@@ -17,10 +15,14 @@ add {
 }
 later(function () require("blink.cmp").setup() end)
 
-add { source = "dgagn/diagflow.nvim" }
-later(function () require("diagflow").setup() end)
+add { source = "numToStr/FTerm.nvim" }
 
-add { source = "mhinz/vim-signify" }
+add {
+  source = "kristijanhusak/vim-dadbod-ui",
+  depends = { "tpope/vim-dadbod", "kristijanhusak/vim-dadbod-completion" },
+}
+vim.g.db_ui_save_location = vim.fn.stdpath "data"
+vim.g.db_ui_use_nerd_fonts = 1
 
 add {
   source = "nvim-treesitter/nvim-treesitter",
@@ -29,13 +31,37 @@ add {
     post_checkout = function () vim.cmd "TSUpdate" end,
   },
 }
+later(function ()
+  require("nvim-treesitter").setup {
+    install_dir = vim.fn.stdpath "data" .. "/site",
+  }
+end)
+
+add { source = "neovim/nvim-lspconfig" }
+later(function ()
+  vim.lsp.enable "stylua"
+  vim.lsp.enable "ruff"
+  vim.lsp.enable "zls"
+end)
+
+add { source = "stevearc/conform.nvim" }
 later(
   function ()
-    require("nvim-treesitter").setup {
-      install_dir = vim.fn.stdpath "data" .. "/site",
+    require("conform").setup {
+      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff" },
+        zig = { "zigfmt" },
+      },
     }
   end
 )
+
+add { source = "dgagn/diagflow.nvim" }
+later(function () require("diagflow").setup() end)
+
+add { source = "mhinz/vim-signify" }
 
 add { source = "kylechui/nvim-surround" }
 later(function () require("nvim-surround").setup() end)
@@ -62,30 +88,15 @@ later(
   end
 )
 
-add { source = "numToStr/FTerm.nvim" }
-
-add { source = "neovim/nvim-lspconfig" }
-later(function ()
-  vim.lsp.enable "stylua"
-  vim.lsp.enable "ruff"
-  vim.lsp.enable "zls"
-end)
-
-add { source = "stevearc/conform.nvim" }
-later(
+add { source = "folke/snacks.nvim" }
+now(
   function ()
-    require("conform").setup {
-      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff" },
-        zig = { "zigfmt" },
-      },
+    require("snacks").setup {
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
     }
   end
 )
 
 add { source = "folke/which-key.nvim" }
-later(
-  function () require("which-key").setup { icons = { mappings = false } } end
-)
+later(function () require("which-key").setup { icons = { mappings = false } } end)
