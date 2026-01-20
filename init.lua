28deada87068b1e342feb2ab517eed0a55e09a8a
ConcreteBridge <conradspark@gmail.com>
2026-01-20 16:21:02 -0600

    update

--- init.lua
+++ init.lua
@@ -1,75 +1,121 @@
---[[ Options ]]
-local g, opt, mapkey = vim.g, vim.opt, vim.keymap.set
+local jetpackfile = vim.fn.stdpath "data" .. "/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim"
+local jetpackurl = "https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim"
+if vim.fn.filereadable(jetpackfile) == 0 then
+  vim.fn.system(string.format("curl -fsSLo %s --create-dirs %s", jetpackfile, jetpackurl))
+end
+
+--[[ OPTIONS ]]
+local g, opt = vim.g, vim.opt
 
 g.mapleader = ";"
 g.maplocalleader = ","
 
 opt.breakindent = true
+opt.breakindentopt = "list:-1"
+opt.complete = { ".", "w", "b", "kspell" }
+opt.completeopt = { "menuone", "noselect", "fuzzy", "popup" }
+opt.cursorline = true
+opt.cursorlineopt = "number"
 opt.expandtab = true
 opt.foldlevel = 99
 opt.gdefault = true
+opt.guifont = "Input Mono Narrow:h15"
 opt.ignorecase = true
 opt.linebreak = true
 opt.list = true
 opt.listchars = { tab = "» ", trail = "•", nbsp = "␣" }
 opt.number = true
+opt.ruler = false
 opt.scrolloff = 3
+opt.shortmess = "CFOSWaco"
 opt.signcolumn = "yes:1"
 opt.smartcase = true
 opt.swapfile = false
+opt.switchbuf = "usetab"
 opt.undofile = true
 opt.wrap = false
 opt.writebackup = false
 
+vim.diagnostic.config {
+  signs = { priority = 9999 },
+  underline = true,
+  virtual_lines = false,
+  virtual_text = false,
+}
+
+--[[ KEYMAPS ]]
+local mapkey = vim.keymap.set
 mapkey("v", "<leader>s", "!sort<cr>", { silent = true })
 mapkey("v", "<leader>u", "!uniq<cr>", { silent = true })
 
---[[ Packages ]]
-local path_package = vim.fn.stdpath "data" .. "/site/"
-local mini_path = path_package .. "pack/deps/start/mini.deps"
-if not vim.loop.fs_stat(mini_path) then
-  vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/nvim-mini/mini.deps", mini_path }
-  vim.cmd "packadd mini.nvim | helptags ALL"
+mapkey("n", "<leader>b", function () require("fzf-lua").buffers() end, { silent = true })
+mapkey("n", "<leader>c", function () require("fzf-lua").grep_cword() end, { silent = true })
+mapkey("n", "<leader>f", function () require("fzf-lua").files() end, { silent = true })
+mapkey("n", "<leader>g", function () require("fzf-lua").live_grep() end, { silent = true })
+mapkey("n", "<leader>l", function () require("fzf-lua").lines() end, { silent = true })
+mapkey("n", "<leader>o", function () require("fzf-lua").oldfiles() end, { silent = true })
+
+mapkey("n", "<leader>d", function () vim.diagnostic.open_float() end, { silent = true })
+
+--[[ PACKAGES ]]
+vim.cmd.packadd "vim-jetpack"
+require "jetpack.paq" {
+  { "tani/vim-jetpack" }, -- bootstrap
+  { "ibhagwan/fzf-lua" },
+  { "jesseleite/nvim-noirbuddy", requires = { "tjdevries/colorbuddy.nvim" } },
+  { "kylechui/nvim-surround" },
+  { "mhinz/vim-signify" },
+  { "neovim/nvim-lspconfig" },
+  { "nvim-treesitter/nvim-treesitter" },
+  { "saghen/blink.cmp", requires = { "rafamadriz/friendly-snippets" } },
+  { "stevearc/conform.nvim" },
+}
+
+local jetpack = require "jetpack"
+for _, name in ipairs(jetpack.names()) do
+  if not jetpack.tap(name) then
+    jetpack.sync()
+    break
+  end
 end
-require("mini.deps").setup { path = { package = path_package } }
 
-local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+require("noirbuddy").setup { preset = "miami-nights" }
 
-add { source = "blazkowolf/gruber-darker.nvim" }
-now(function () vim.cmd.colorscheme "gruber-darker" end)
+local tree = require "nvim-treesitter"
+tree.setup { install_dir = vim.fn.stdpath "data" .. "/site" }
+tree.install { "c", "lua" }
 
-add { source = "mhinz/vim-signify" }
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "lua" },
+  callback = function () vim.treesitter.start() end,
+})
 
-add { source = "ibhagwan/fzf-lua" }
-now(function ()
-  local fzf = require "fzf-lua"
-  fzf.setup { "ivy", fzf_colors = true, winopts = { border = "none", preview = { hidden = true } } }
-  mapkey("n", "<leader>b", function () fzf.buffers() end, { silent = true })
-  mapkey("n", "<leader>f", function () fzf.files() end, { silent = true })
-  mapkey("n", "<leader>g", function () fzf.live_grep() end, { silent = true })
-  mapkey("n", "<leader>l", function () fzf.lines() end, { silent = true })
-  mapkey("n", "<leader>o", function () fzf.oldfiles() end, { silent = true })
-end)
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldmethod = "expr"
+vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
 
-add { source = "kylechui/nvim-surround" }
-later(function () require("nvim-surround").setup() end)
+require("fzf-lua").setup {
+  "ivy",
+  fzf_colors = true,
+  winopts = { border = "none", preview = { hidden = true } },
+}
 
-add {
-  source = "nvim-treesitter/nvim-treesitter",
-  checkout = "main",
-  hooks = {
-    post_checkout = function () vim.cmd "TSUpdate" end,
+-- vim.lsp.enable "clangd"
+
+require("blink.cmp").setup {
+  fuzzy = { implementation = "lua" },
+}
+
+require("nvim-surround").setup()
+
+require("conform").setup {
+  format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+  formatters = {
+    ["clang-format"] = { args = { "--style=WebKit", "-assume-filename", "$FILENAME" } },
+  },
+  formatters_by_ft = {
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+    lua = { "stylua" },
   },
 }
-now(function ()
-  local tree = require "nvim-treesitter"
-  tree.setup { install_dir = vim.fn.stdpath "data" .. "/site" }
-  tree.install { "all" }
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = tree.get_installed(),
-    callback = function () vim.treesitter.start() end,
-  })
-  vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-  vim.wo.foldmethod = "expr"
-  vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-end)
